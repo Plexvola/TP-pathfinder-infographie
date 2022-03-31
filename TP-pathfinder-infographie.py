d5877461f70e3427ce9799bc59eda2e76354229e
Plex <thinkplex@riseup.net>
2022-03-31 23:33:31 +0200

    classe Grille

--- TP-pathfinder-infographie.py
+++ TP-pathfinder-infographie.py
@@ -7,46 +7,138 @@ from tkinter import messagebox
 # ----- Variables globales ----- #
 taille_case = 30
 nombre_cases = 30  # Nombre de cases par ligne et par colonne
-cases = []  # Liste contenant les objets cases
-poids = []  # Liste contenant les nombres dans les cases
-case_depart = False  # Variable qui detecte la presence d'une case depart
-case_fin = False  # Variable qui detecte la presence d'une case fin
-total = 0  # total du chemin
 
 
 def rgb_hack(rgb):
     """Transforme un nombre en sa représentation RGB."""
     return "#%02x%02x%02x" % rgb
 
-class Case:
-    """Classe de la case."""
 
-    def __init__(self, x, y, poids, C):
+class Case:
+    """Case unique, avec un poids et une position."""
+
+    def __init__(self, x, y, poids):
         """Initialise la case."""
         self.x = x
         self.y = y
         self.couleur = rgb_hack((255, 255 - 15 * poids, 255))
         self.poids = poids
-        self.canvas = C
+        self.etat = "VIDE"
 
-    def render(self, taille_case):
+    def render(self, taille_case, canvas):
         """Affiche la case à l'écran."""
-        self.id = self.canvas.create_rectangle(
+        self.id = canvas.create_rectangle(
             self.x * taille_case + 2,
             self.y * taille_case + 2,
             (self.x + 1) * taille_case + 2,
             (self.y + 1) * taille_case + 2
         )
-        self.canvas.create_text(
+        canvas.create_text(
             self.x * taille_case + taille_case/2,
             self.y * taille_case + taille_case/2,
             text=self.poids
         )
-        self.canvas.itemconfigure(
+        canvas.itemconfigure(
             self.id, outline="black",
             fill=self.couleur,
         )
 
+    def depart(self, canvas):
+        """Définit la case comme la case de départ."""
+        canvas.itemconfigure(
+            self.id, outline="black",
+            fill=rgb_hack((0, 255, 0))
+        )
+        self.etat = "DEPART"
+
+    def arrivee(self, canvas):
+        """Définit la case comme la case d'arrivée."""
+        canvas.itemconfigure(
+            self.id, outline="black",
+            fill=rgb_hack((255, 0, 0))
+        )
+        self.etat = "ARRIVEE"
+
+    def traverser(self, canvas):
+        """Traverse la case."""
+        canvas.itemconfigure(
+            self.id, outline="black",
+            fill=rgb_hack((255, 128, 0))
+        )
+        self.etat = "CHEMIN"
+
+
+class Grille:
+    """Grille composée de cases."""
+
+    def __init__(self, taille, dimension, canvas):
+        """Crée une grille contenant des cases.
+
+        Chaque case mesure taille, et la grille possède dimension cases par
+        côté.
+        """
+        self.taille = taille
+        self.dim = dimension
+        self.cases = [[] for _ in range(dimension)]
+        self.total = 0
+        self.canvas = canvas
+
+        self.case_d = False
+        self.case_a = False
+
+        for ligne in range(dimension):
+            for colonne in range(dimension):
+                case = Case(ligne, colonne, randrange(1, 17))
+                case.render(taille, canvas)
+                self.cases[ligne].append(case)
+
+    def depart(self, event):
+        """Place la case de départ à l'endroit cliqué."""
+        x = event.x // self.taille
+        y = event.y // self.taille
+        self.cases[x][y].depart(self.canvas)
+        self.total += self.cases[x][y].poids
+
+    def arrivee(self, event):
+        """Place la case de fin à l'endroit cliqué."""
+        x = event.x // self.taille
+        y = event.y // self.taille
+        self.cases[x][y].arrivee(self.canvas)
+        self.total += self.cases[x][y].poids
+
+    def chemin(self, event):
+        """Place une case de chemin à l'endroit cliqué."""
+        x = event.x // self.taille
+        y = event.y // self.taille
+        if self.cases[x][y].etat == "ARRIVEE":
+            print("vous etes arrivé, vous avez parcouru un total de",
+                  self.total)
+        elif self.cases[x][y].etat == "DEPART":
+            messagebox.showwarning(
+                title="case depart", message="ceci est la case de depart"
+            )
+        elif self.cases[x][y].etat == "CHEMIN":
+            messagebox.showwarning(
+                title="case depart", message="vous êtes deja passer par là"
+            )
+        else:
+            self.cases[x][y].traverser(self.canvas)
+            self.total += self.cases[x][y].poids
+
+    def clic_case(self, event):
+        """Cliquer sur une case."""
+        if self.case_d:
+            if self.case_a:
+                self.chemin(event)
+            else:
+                self.arrivee(event)
+                self.case_a = True
+        else:
+            self.depart(event)
+            self.case_d = True
+
+
+# ----- Programme principal ----- #
 
 # ----- Création de la fenêtre ----- #
 fen = tk.Tk()
@@ -58,83 +150,10 @@ canvas_cases = tk.Canvas(fen, width=nombre_cases * taille_case,
 canvas_cases.grid(row=0, column=0, columnspan=2, padx=3, pady=3)
 
 # ----- Création des figures ----- #
-for ligne in range(nombre_cases):
-    cases.append([])
-    for colonne in range(nombre_cases):  # Conception des cases d'une ligne
-        case = Case(ligne, colonne, randrange(1, 17), canvas_cases)
-        case.render(taille_case)
-        cases[ligne].append(case)
+g = Grille(30, 10, canvas_cases)
 
+fen.bind("<Button-1>", g.clic_case)
 
-# ----- Création des nombres dans les cases colonne par colonne ----- #
-
-
-def depart(event):
-    """Place la case de départ à l'endroit cliqué."""
-    global total
-    x = event.x
-    y = event.y
-    canvas_cases.itemconfigure(
-        cases[x // taille_case][y // taille_case].id, outline="black",
-        fill=rgb_hack((0, 255, 0))
-    )
-    total += cases[x // taille_case][y // taille_case].poids
-
-
-def fin(event):
-    """Place la case de fin à l'endroit cliqué."""
-    global total
-    x = event.x
-    y = event.y
-    canvas_cases.itemconfigure(
-        cases[x // taille_case][y // taille_case].id, outline="black",
-        fill=rgb_hack((255, 0, 0))
-    )
-    total += cases[x // taille_case][y // taille_case].poids
-
-def chemin(event):
-    """Place une case de chemin à l'endroit cliqué."""
-    global total
-    x = event.x
-    y = event.y
-    if canvas_cases.itemcget(cases[x // taille_case][y // taille_case].id,
-                             "fill") == rgb_hack((255, 0, 0)):
-        print("vous etes arrivé, vous avez parcouru un total de", total)
-    elif canvas_cases.itemcget(cases[x // taille_case][y // taille_case].id,
-                               "fill") == rgb_hack((0, 255, 0)):
-        messagebox.showwarning(
-            title="case depart", message="ceci est la case de depart"
-        )
-    elif canvas_cases.itemcget(cases[x // taille_case][y // taille_case].id,
-                               "fill") == rgb_hack((255, 128, 0)):
-        messagebox.showwarning(
-            title="case depart", message="vous êtes deja passer par là"
-        )
-    else:
-        canvas_cases.itemconfigure(
-            cases[x // taille_case][y // taille_case].id, outline="black",
-            fill=rgb_hack((255, 128, 0))
-        )
-        total += cases[x // taille_case][y // taille_case].poids
-
-
-def clic_case(event):
-    """Cliquer sur une case."""
-    global case_depart, case_fin
-    if case_depart:
-        if case_fin:
-            chemin(event)
-        else:
-            fin(event)
-            case_fin = True
-    else:
-        depart(event)
-        case_depart = True
-
-
-fen.bind("<Button-1>", clic_case)
-
-# ----- Programme principal ----- #
 fen.mainloop()  # Boucle d'attente des événements
 
 
