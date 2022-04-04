fce597268239bc331d18bedd3985bb13054cd98e
Plex <thinkplex@riseup.net>
2022-04-04 14:10:56 +0200

    bordure

--- TP-pathfinder-infographie.py
+++ TP-pathfinder-infographie.py
@@ -25,46 +25,41 @@ class Case:
         self.poids = poids
         self.etat = "VIDE"
 
+    def __str__(self):
+        return str(self.x) + ", " + str(self.y) + ": " + str(self.poids)
+
     def render(self, taille_case, canvas):
         """Affiche la case à l'écran."""
         self.id = canvas.create_rectangle(
             self.x * taille_case + 2,
             self.y * taille_case + 2,
             (self.x + 1) * taille_case + 2,
-            (self.y + 1) * taille_case + 2
+            (self.y + 1) * taille_case + 2,
         )
         canvas.create_text(
-            self.x * taille_case + taille_case/2,
-            self.y * taille_case + taille_case/2,
-            text=self.poids
+            self.x * taille_case + taille_case / 2,
+            self.y * taille_case + taille_case / 2,
+            text=self.poids,
         )
         canvas.itemconfigure(
-            self.id, outline="black",
+            self.id,
+            outline="black",
             fill=self.couleur,
         )
 
     def depart(self, canvas):
         """Définit la case comme la case de départ."""
-        canvas.itemconfigure(
-            self.id, outline="black",
-            fill=rgb_hack((0, 255, 0))
-        )
+        canvas.itemconfigure(self.id, outline="black", fill=rgb_hack((0, 255, 0)))
         self.etat = "DEPART"
 
     def arrivee(self, canvas):
         """Définit la case comme la case d'arrivée."""
-        canvas.itemconfigure(
-            self.id, outline="black",
-            fill=rgb_hack((255, 0, 0))
-        )
+        canvas.itemconfigure(self.id, outline="black", fill=rgb_hack((255, 0, 0)))
         self.etat = "ARRIVEE"
 
     def traverser(self, canvas):
         """Traverse la case."""
-        canvas.itemconfigure(
-            self.id, outline="black",
-            fill=rgb_hack((255, 128, 0))
-        )
+        canvas.itemconfigure(self.id, outline="black", fill=rgb_hack((255, 128, 0)))
         self.etat = "CHEMIN"
 
 
@@ -89,12 +84,16 @@ class Grille:
         self.case_d = False
         self.case_a = False
 
-        self.cases = [[] for _ in range(self.dim)]
-        for ligne in range(self.dim):
-            for colonne in range(self.dim):
+        self.cases = [[] for _ in range(self.dim+2)]
+        self.cases[0] = [Case(0, n, 1000) for n in range(self.dim+2)]
+        self.cases[-1] = [Case(self.dim+1, n, 1000) for n in range(self.dim+2)]
+        for ligne in range(1, self.dim+1):
+            self.cases[ligne].append(Case(ligne, 0, 1000))
+            for colonne in range(1, self.dim+1):
                 case = Case(ligne, colonne, randrange(1, 17))
                 case.render(self.taille, self.canvas)
                 self.cases[ligne].append(case)
+            self.cases[ligne].append(Case(ligne, self.dim+2, 1000))
 
     def depart(self, event):
         """Place la case de départ à l'endroit cliqué."""
@@ -115,8 +114,7 @@ class Grille:
         x = event.x // self.taille
         y = event.y // self.taille
         if self.cases[x][y].etat == "ARRIVEE":
-            print("vous etes arrivé, vous avez parcouru un total de",
-                  self.total)
+            print("vous etes arrivé, vous avez parcouru un total de", self.total)
         elif self.cases[x][y].etat == "DEPART":
             messagebox.showwarning(
                 title="case depart", message="ceci est la case de depart"
@@ -149,18 +147,23 @@ fen = tk.Tk()
 fen.title("Chemin")
 
 # ----- Création des canvas ----- #
-canvas_cases = tk.Canvas(fen, width=nombre_cases * taille_case,
-                         height=nombre_cases * taille_case, bg="white")
+canvas_cases = tk.Canvas(
+    fen, width=nombre_cases * taille_case, height=nombre_cases * taille_case, bg="white"
+)
 canvas_cases.grid(row=0, column=0, columnspan=2, padx=3, pady=3)
 
 # ----- Création des figures ----- #
 g = Grille(taille_case, nombre_cases, canvas_cases)
+for li in g.cases:
+    for col in li:
+        print(col, end=' | ')
+    print("")
 
 
 frame_bouton = tk.Frame(fen, height=50, width=100)
 frame_bouton.grid()
 
-GenButton = tk.Button(fen, text='Generate', command=g.generate)
+GenButton = tk.Button(fen, text="Generate", command=g.generate)
 GenButton.grid()
 
 fen.bind("<Button-3>", g.clic_case)
